f17cbc143d0a4ffa1ce87e8aec4bd311342909ba
PritikaRamu <60213317+PritikaRamu@users.noreply.github.com>
2020-08-16 20:54:25 +0530

    Sending score

--- public/mastermind/mastermind.js
+++ public/mastermind/mastermind.js
@@ -56,7 +56,11 @@ function show_instructions(){
   function win() {
   $('.modal1').fadeIn(200);
   clearInterval(x);
-  fetch("/score",{method:"POST",body:JSON.stringify({score:100})})
+  fetch("/score",
+        {method:"POST",
+         body:JSON.stringify({score:100}),
+         headers:{"Content-type": "application/json; charset=UTF-8"}
+         )
 }
 
   //loss modal
